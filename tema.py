405222fb0a9f4c6bba145cfc6594e6b31c0693f4
danielrpopa <87667159+danielrpopa@users.noreply.github.com>
2021-08-01 19:18:16 +0300

    Update tema.py

--- tema.py
+++ tema.py
@@ -1,9 +1,10 @@
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 print('Initial list', my_list)
-my_list.sort()
-print('Sorted list:', my_list)
-print('my_even_list', my_list[1::2])
-print('Reverse list', my_list[::-1])
-print('my_odd_list', my_list[::2])
-my_sliced_list = my_list[2::3]
+new_list = sorted(my_list)
+print('Sorted list', new_list)
+print('my_even_list', new_list[1::2])
+print('Reverse list', new_list[::-1])
+print('my_odd_list', new_list[::2])
+my_sliced_list = new_list[2::3]
 print('Multiple of 3', my_sliced_list)
+print('Initial list', my_list)
